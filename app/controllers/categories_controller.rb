67e8f525108c39c6c4dc9b3ff2c50d731afaa01d
Alex Younger <alex@izoox.com>
2019-12-08 01:46:23 -0700

    Ajax working

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,12 +2,18 @@ class CategoriesController < ApplicationController
     before_action :set_category, only: [:edit, :destroy]
 
     def create    
-        @category = Category.new
-        @category.name = params["name"]
-        @category.color = params["color"]
-        selector = (params["name"]).gsub(/\s+/, "").downcase.to_s
-        @category.selector = selector
-        @category.save!
+        @category = Category.new(
+            name: params["name"],
+            color: params["color"],
+            selector: (params["name"]).gsub(/\s+/, "").downcase.to_s,
+        )
+        if @category.save!
+            respond_to do |format|  
+                format.js
+                format.html { redirect_to @category, notice: 'Item successfully created' }
+                format.json { render "items/create", status: :created, location: @category }
+            end        
+        end
     end
 
     private
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,7 +13,7 @@ class ItemsController < ApplicationController
             respond_to do |format|  
                 format.js
                 format.html { redirect_to @item, notice: 'Item successfully created' }
-                format.json { render "items/show", status: :created, location: @item }
+                format.json { render "items/create", status: :created, location: @item }
             end        
         end
     end
